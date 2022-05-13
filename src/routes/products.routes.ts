98694017bcd5d521652929df37d616725e8eba0d
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-05-13 16:31:05 -0300

    create: create products route

--- src/routes/products.routes.ts
+++ src/routes/products.routes.ts
@@ -1,8 +1,11 @@
 import express from 'express';
 import productsController from '../controllers/products.controller';
+import productName from '../middlewares/productName.middlewares';
+import productAmount from '../middlewares/productAmount.middleware';
 
 const route = express();
 
-route.get('/', productsController);
+route.get('/', productsController.getAll);
+route.post('/', productName, productAmount, productsController.create);
 
 export default route;
