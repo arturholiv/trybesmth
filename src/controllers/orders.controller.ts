9a28b5c1b399b8b49501da40e291fdc169b9d413
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-05-13 19:49:09 -0300

    fix req 4

--- src/controllers/orders.controller.ts
+++ src/controllers/orders.controller.ts
@@ -4,7 +4,7 @@ import orderModel from '../models/orders.model';
 const getAllOrders = async (req: Request, res: Response, next: NextFunction) => {
   try {
     const response = await orderModel();
-    return res.status(201).json(response);
+    return res.status(200).json(response);
   } catch (e) {
     return next(e);
   }
