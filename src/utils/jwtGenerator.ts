807431e16e8df5d75999876a21ac616e0267e565
Artur Henrique de Oliveira <artur.henrique45@hotmail.com>
2022-05-13 18:10:45 -0300

    jwt generator

---
+++ src/utils/jwtGenerator.ts
@@ -0,0 +1,18 @@
+import jwt, { Secret, SignOptions } from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const secret = process.env.JWT_SECRET as Secret;
+
+const generate = (payload: object) => {
+  const options: SignOptions = {
+    algorithm: 'HS256',
+    expiresIn: '20 min',
+  };
+
+  const token = jwt.sign(payload, secret, options);
+  return token;
+};
+
+export default generate;
